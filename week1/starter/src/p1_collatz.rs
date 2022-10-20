59500eaa7bb25359333d872189d6490bbca567b6
Alexander Ding <alexander.jf.ding@gmail.com>
2022-10-20 17:13:41 -0400

    feat: finish week 1 assignment

--- week1/starter/src/p1_collatz.rs
+++ week1/starter/src/p1_collatz.rs
@@ -12,40 +12,50 @@
 ///
 /// Run `cargo test collatz_recursive_test` to check your answer.
 pub fn collatz_recursive(n: usize) -> usize {
-  unimplemented!()
+    if n == 1 {
+        0
+    } else {
+        1 + collatz_recursive(if n % 2 == 0 { n / 2 } else { 3 * n + 1 })
+    }
 }
 
 /// Problem 1b: write an **ITERATIVE** function that computes the value of i for a given n.
 ///
 /// Run `cargo test collatz_iterative_test` to check your answer.
 pub fn collatz_iterative(n: usize) -> usize {
-  unimplemented!()
+    let mut count: usize = 0;
+    let mut n = n;
+    while n != 1 {
+        n = if n % 2 == 0 { n / 2 } else { 3 * n + 1 };
+        count += 1;
+    }
+    count
 }
 
 #[cfg(test)]
 mod test {
-  use super::*;
-  const COLLATZ_ANSWERS: [usize; 10] = [0, 1, 7, 2, 5, 8, 16, 3, 19, 6];
+    use super::*;
+    const COLLATZ_ANSWERS: [usize; 10] = [0, 1, 7, 2, 5, 8, 16, 3, 19, 6];
 
-  #[test]
-  fn collatz_recursive_test() {
-    for (n, answer) in COLLATZ_ANSWERS
-      .into_iter()
-      .enumerate()
-      .map(|(i, a)| (i + 1, a))
-    {
-      assert_eq!(collatz_recursive(n), answer, "n = {}", n);
+    #[test]
+    fn collatz_recursive_test() {
+        for (n, answer) in COLLATZ_ANSWERS
+            .into_iter()
+            .enumerate()
+            .map(|(i, a)| (i + 1, a))
+        {
+            assert_eq!(collatz_recursive(n), answer, "n = {}", n);
+        }
     }
-  }
 
-  #[test]
-  fn collatz_iterative_test() {
-    for (n, answer) in COLLATZ_ANSWERS
-      .into_iter()
-      .enumerate()
-      .map(|(i, a)| (i + 1, a))
-    {
-      assert_eq!(collatz_iterative(n), answer, "n = {}", n);
+    #[test]
+    fn collatz_iterative_test() {
+        for (n, answer) in COLLATZ_ANSWERS
+            .into_iter()
+            .enumerate()
+            .map(|(i, a)| (i + 1, a))
+        {
+            assert_eq!(collatz_iterative(n), answer, "n = {}", n);
+        }
     }
-  }
 }
